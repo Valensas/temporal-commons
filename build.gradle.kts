ac1bbbdb86c1ad46df00e4d97cc273937781eef9
Betül Demirtaş <betul.demirtas@valensas.com>
2026-01-05 16:32:29 +0300

    add schedule client bean

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "com.valensas"
-version = "1.0.1"
+version = "1.0.2"
 
 java {
     toolchain {
--- src/main/kotlin/com/valensas/temporalcommons/client/TemporalClientConfiguration.kt
+++ src/main/kotlin/com/valensas/temporalcommons/client/TemporalClientConfiguration.kt
@@ -3,6 +3,7 @@ package com.valensas.temporalcommons.client
 import com.valensas.temporalcommons.config.TemporalProperties
 import io.temporal.client.WorkflowClient
 import io.temporal.client.WorkflowClientOptions
+import io.temporal.client.schedules.ScheduleClient
 import io.temporal.common.converter.DataConverter
 import io.temporal.serviceclient.WorkflowServiceStubs
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
@@ -27,4 +28,8 @@ class TemporalClientConfiguration {
 
         return WorkflowClient.newInstance(serviceStubs, options)
     }
+
+    @Bean
+    @ConditionalOnMissingBean
+    fun scheduleClient(serviceStubs: WorkflowServiceStubs): ScheduleClient = ScheduleClient.newInstance(serviceStubs)
 }
